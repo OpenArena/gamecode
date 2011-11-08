27bb968497a098246562821e7e0e11f0df253db0
sago007 <sago007@48ef5666-4144-11de-b6cc-f32266f7ba4a>
2011-11-08 20:39:18 +0000

    Changed version number in the cornor
    git-svn-id: http://oax.googlecode.com/svn/branches/oa-0.8.8@282 48ef5666-4144-11de-b6cc-f32266f7ba4a

--- code/q3_ui/ui_menu.c
+++ code/q3_ui/ui_menu.c
@@ -239,7 +239,7 @@ static void Main_MenuDraw( void ) {
 		UI_DrawString( 320, 444, "read COPYING for details.", UI_CENTER|UI_SMALLFONT, color );
                 
                 //Draw version.
-                UI_DrawString( 640-40, 480-14, "^7OAX", UI_SMALLFONT, color );
+                UI_DrawString( 640-40, 480-14, "^70.8.8", UI_SMALLFONT, color );
                 if((int)trap_Cvar_VariableValue("protocol")!=71)
                     UI_DrawString( 0, 480-14, va("^7Protocol: %i",(int)trap_Cvar_VariableValue("protocol")), UI_SMALLFONT, color);
 }
