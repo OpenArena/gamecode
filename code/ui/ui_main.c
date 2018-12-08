df03946aa8b0441ff17b3a3fc9cb8f41273db9a3
Neon_Knight <openarena.arg@gmail.com>
2018-12-08 19:09:05 -0300

    Backport from ioq3: 10abac8 "Fix duplicate bots displayed in Team Arena ingame add bot menu"

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -2416,17 +2416,11 @@ static void UI_DrawBotName(rectDef_t *rect, float scale, vec4_t color, int textS
 {
 	int value = uiInfo.botIndex;
 	int game = trap_Cvar_VariableValue("g_gametype");
-	const char *text = "";
+	const char *text;
 	if (UI_IsATeamGametype(game)) {
-		if (value >= uiInfo.characterCount) {
-			value = 0;
-		}
 		text = uiInfo.characterList[value].name;
 	}
 	else {
-		if (value >= UI_GetNumBots()) {
-			value = 0;
-		}
 		text = UI_GetBotNameByNumber(value);
 	}
 	Text_Paint(rect->x, rect->y, scale, color, text, 0, 0, textStyle);
@@ -3352,19 +3346,19 @@ static qboolean UI_BotName_HandleKey(int flags, float *special, int key)
 		value += select;
 
 		if (UI_IsATeamGametype(game)) {
-			if (value >= uiInfo.characterCount + 2) {
+			if (value >= uiInfo.characterCount) {
 				value = 0;
 			}
 			else if (value < 0) {
-				value = uiInfo.characterCount + 2 - 1;
+				value = uiInfo.characterCount - 1;
 			}
 		}
 		else {
-			if (value >= UI_GetNumBots() + 2) {
+			if (value >= UI_GetNumBots()) {
 				value = 0;
 			}
 			else if (value < 0) {
-				value = UI_GetNumBots() + 2 - 1;
+				value = UI_GetNumBots() - 1;
 			}
 		}
 		uiInfo.botIndex = value;
