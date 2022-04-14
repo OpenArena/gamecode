1b3e1dcd40124ff4462fc46b17df1ccf4af5a136
NeonKnightOA <openarena.arg@gmail.com>
2022-04-14 12:33:15 -0300

    Seapration of UI_DrawTeamMember into UI_DrawTeamMemberBlue and UI_DrawTeamMemberRed

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -1590,12 +1590,44 @@ static void UI_DrawTeamNameBlue(rectDef_t *rect, float scale, vec4_t color, int 
 	}
 }
 
-static void UI_DrawTeamMember(rectDef_t *rect, float scale, vec4_t color, qboolean blue, int num, int textStyle)
+static void UI_DrawTeamMemberBlue(rectDef_t *rect, float scale, vec4_t color, int num, int textStyle)
 {
 	// 0 - None
 	// 1 - Human
 	// 2..NumCharacters - Bot
-	int value = trap_Cvar_VariableValue(va(blue ? "ui_blueteam%i" : "ui_redteam%i", num));
+	int value = trap_Cvar_VariableValue(va("ui_blueteam%i", num));
+	const char *text;
+	if (value <= 0) {
+		text = "Closed";
+	}
+	else if (value == 1) {
+		text = "Human";
+	}
+	else {
+		value -= 2;
+
+		if (UI_IsATeamGametype(ui_actualNetGameType.integer)) {
+			if (value >= uiInfo.characterCount) {
+				value = 0;
+			}
+			text = uiInfo.characterList[value].name;
+		}
+		else {
+			if (value >= UI_GetNumBots()) {
+				value = 0;
+			}
+			text = UI_GetBotNameByNumber(value);
+		}
+	}
+	Text_Paint(rect->x, rect->y, scale, color, text, 0, 0, textStyle);
+}
+
+static void UI_DrawTeamMemberRed(rectDef_t *rect, float scale, vec4_t color, int num, int textStyle)
+{
+	// 0 - None
+	// 1 - Human
+	// 2..NumCharacters - Bot
+	int value = trap_Cvar_VariableValue(va("ui_redteam%i", num));
 	const char *text;
 	if (value <= 0) {
 		text = "Closed";
@@ -2749,14 +2781,14 @@ static void UI_OwnerDraw(float x, float y, float w, float h, float text_x, float
 	case UI_BLUETEAM3:
 	case UI_BLUETEAM4:
 	case UI_BLUETEAM5:
-		UI_DrawTeamMember(&rect, scale, color, qtrue, ownerDraw - UI_BLUETEAM1 + 1, textStyle);
+		UI_DrawTeamMemberBlue(&rect, scale, color, ownerDraw - UI_BLUETEAM1 + 1, textStyle);
 		break;
 	case UI_REDTEAM1:
 	case UI_REDTEAM2:
 	case UI_REDTEAM3:
 	case UI_REDTEAM4:
 	case UI_REDTEAM5:
-		UI_DrawTeamMember(&rect, scale, color, qfalse, ownerDraw - UI_REDTEAM1 + 1, textStyle);
+		UI_DrawTeamMemberRed(&rect, scale, color, ownerDraw - UI_REDTEAM1 + 1, textStyle);
 		break;
 	case UI_NETSOURCE:
 		UI_DrawNetSource(&rect, scale, color, textStyle);
