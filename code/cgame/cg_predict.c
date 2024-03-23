373cbb95809e5807796b89ea24ebe18513c02019
NeonKnightOA <openarena.arg@gmail.com>
2024-03-23 23:07:28 +0000

    Genericizing a gametype call in CG_TouchItem and putting inside of it the per-gametype cases. (#281)

--- code/cgame/cg_predict.c
+++ code/cgame/cg_predict.c
@@ -296,18 +296,16 @@ static void CG_TouchItem( centity_t *cent ) {
 
 	// Special case for flags.  
 	// We don't predict touching our own flag
-	if( cgs.gametype == GT_1FCTF ) {
-		if( item->giType == IT_TEAM && item->giTag != PW_NEUTRALFLAG ) {
+	if(CG_UsesTheWhiteFlag(cgs.gametype) && item->giType == IT_TEAM && item->giTag != PW_NEUTRALFLAG) {
+		if( cgs.gametype == GT_1FCTF ) {
 			return;
 		}
-	}
-	if (cgs.gametype == GT_POSSESSION) {
-		if( item->giType == IT_TEAM && item->giTag == PW_NEUTRALFLAG ) {
+		if (cgs.gametype == GT_POSSESSION) {
 			canBePicked = qtrue;
 		}
 	}
-	
-	if (cgs.gametype == GT_CTF || cgs.gametype == GT_CTF_ELIMINATION ) {
+
+	if (CG_UsesTeamFlags(cgs.gametype)) {
 		if (cg.predictedPlayerState.persistant[PERS_TEAM] == TEAM_RED &&
 			item->giType == IT_TEAM && item->giTag == PW_REDFLAG)
 			return;
